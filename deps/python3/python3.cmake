04fed02313d6d5293ee661d249f7a4b6cc227e9c
raistlin7447 <kris.austin@gmail.com>
2026-09-14 21:36:41 -0500

    wip: compile unicodectype.c unoptimised on arm64 (to be reworded)

--- deps/python3/python3.cmake
+++ deps/python3/python3.cmake
@@ -88,8 +88,18 @@ if(WIN32)
         list(APPEND _python_env_args "PreferredToolArchitecture=${_python_tool_arch}")
     endif()
 
+    # PCbuild/msbuild.rsp carries extra MSBuild switches; build.bat picks it up.
+    set(_python_rsp "/p:PlatformToolset=${_python_platform_toolset}\n")
+    # VS 2026's ARM64 code generator needs about 27 GB for one function in
+    # Objects/unicodectype.c, more than a 16 GB machine can commit. Drop this
+    # once the compiler is fixed.
+    if(_python_pcbuild_platform STREQUAL "ARM64")
+        file(TO_NATIVE_PATH "${CMAKE_CURRENT_LIST_DIR}/arm64-unicodectype.props" _python_arm64_props)
+        string(APPEND _python_rsp "/p:ForceImportAfterCppTargets=\"${_python_arm64_props}\"\n")
+    endif()
+    file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/python3-msbuild.rsp" "${_python_rsp}")
     set(_conf_cmd
-        cmd /c "echo /p:PlatformToolset=${_python_platform_toolset}>PCbuild\\msbuild.rsp"
+        ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_BINARY_DIR}/python3-msbuild.rsp" <SOURCE_DIR>/PCbuild/msbuild.rsp
     )
     set(_build_cmd
         ${CMAKE_COMMAND} -E env ${_python_env_args}
